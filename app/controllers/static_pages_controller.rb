0d64e6e1c1b08ec38048dcec6a29ae9553a3ceba
Strawberry <strawberrycsgo@gmail.com>
2017-11-09 12:43:39 +0700

    ajax in home

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def show
     @q = Review.ransack params[:q]
-    @q.sorts = %w(title created_at desc) if @q.sorts.empty?
+    # @q.sorts = %w(title created_at desc) if @q.sorts.empty?
     @reviews = @q.result.page(params[:page]).per 9
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks"}
   resources :users
-  root "static_pages#show", page: "home"
+  root "static_pages#show"
   get "/pages/:page" => "static_pages#show"
   resources :reviews
   resources :comments
