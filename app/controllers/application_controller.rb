4d9b74fb8a7e035b45d553cef74c53fcecd900d0
phanmanhcuong <phancuong1203@gmail.com>
2017-11-23 12:39:39 +0700

    nihongo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
 
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :address, :birthday, :avatar])
+  end
+
+  def set_locale
+  	I18n.locale = params[:locale] || I18n.default_locale	
   end
 end
